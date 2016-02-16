4ee92786a233c9461606a15e5f1b90d3ee84b2d2
valdi1212 <valdi1212@gmail.com>
2016-02-16 12:31:36 +0000

    work done at school

--- Verkefni3.sql
+++ Verkefni3.sql
@@ -14,9 +14,15 @@ begin
 	
 	declare done int default false;
 	
-	declare passengerListCursor cursor
-		for select personID, personName, rowNumber, seatNumber, seatPlacement
-		from passengers join aircraftSeats on passengers.seatID = aircraftSeats.seatID;
+	declare passengerListCursor cursor for 
+		select personID, personName, rowNumber, seatNumber, seatPlacement
+		from passengers 
+		join aircraftSeats on passengers.seatID = aircraftSeats.seatID
+		join aircrafts on aircraftSeats.aircraftID = aircrafts.aircraftID
+		join flights on aircrafts.aircraftID = flights.aircraftID
+		where flight_Number = flights.flightNumber AND flight_date = flights.flightDate;
+	
+	declare continue handler for not found set done = true;
 		
 	select originatingAirport 
 	from flightSchedules
@@ -28,10 +34,16 @@ begin
 	where flightNumber = flight_number
 	into flight_destination;
 	
-	set cvs_string = concat(flight_number, '_', flight_origin, '-', flight_destination, '_', flight_date, ':');
+	set cvs_string = concat(flight_number, '_', flight_origin, '-', flight_destination, '_', flight_date, ':\n\n');
 	
-	select cvs_string;
+	open passengerListCursor;
+	
+	read_loop: loop
+		fetch passengerListCursor
+		into -- declare the variables at the top...
+	
+	--select cvs_string;
 end $$
 delimiter ;
 
--- CALL PassengerList('FA501', '19-09-2014');
+-- CALL PassengerList('FA501', '2014-09-19');
