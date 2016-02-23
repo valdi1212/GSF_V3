abd1a17f0e920956e688a8e42049034aab2098a4
valdi1212 <valdi1212@gmail.com>
2016-02-23 17:35:38 +0000

    work done at home+

--- Verkefni3.sql
+++ Verkefni3.sql
@@ -2,65 +2,71 @@ USE 1212962259_FreshAir_International;
 
 -- Stored Procedures
 
-delimiter $$
-drop procedure if exists PassengerList $$
-create procedure PassengerList(flight_number CHAR(5), flight_date DATE)
-begin
+DELIMITER $$
+DROP PROCEDURE IF EXISTS PassengerList $$
+CREATE PROCEDURE PassengerList(flight_number CHAR(5), flight_date DATE)
+  BEGIN
 
-	-- variables for the header
-	declare flight_origin char(3);
-	declare flight_destination char(3);
-	
-	-- variables for the cursor
-	declare person_id varchar(35);
-	declare person_name varchar(75);
-	declare seat_row tinyint(4);
-	declare seat_num char(1);
-	declare seat_placement varchar(15);
+    -- variables for the header
+    DECLARE flight_origin CHAR(3);
+    DECLARE flight_destination CHAR(3);
 
-	declare cvs_string text;
-	
-	declare done int default false;
-	
-	declare passengerListCursor cursor for 
-		select personID, personName, rowNumber, seatNumber, seatPlacement
-		from passengers 
-		join aircraftSeats on passengers.seatID = aircraftSeats.seatID
-		join aircrafts on aircraftSeats.aircraftID = aircrafts.aircraftID
-		join flights on aircrafts.aircraftID = flights.aircraftID
-		where flight_Number = flights.flightNumber AND flight_date = flights.flightDate;
-	
-	declare continue handler for not found set done = true;
-		
-	select originatingAirport 
-	from flightSchedules
-	where flightNumber = flight_number
-	into flight_origin;
-	
-	select destinationAirport
-	from flightSchedules
-	where flightNumber = flight_number
-	into flight_destination;
-	
-	set cvs_string = concat(flight_number, '_', flight_origin, '-', flight_destination, '_', flight_date, ':\n\n');
-	
-	open passengerListCursor;
-	
-	read_loop: loop
-		fetch passengerListCursor
-		into person_id, person_name, seat_row, seat_num, seat_placement;
-		
-		set cvs_string = concat(cvs_string, person_id, ';', person_name, ';', seat_row, seat_num, ';', seat_placement, ';\n');
-		
-		if done
-		then
-			leave read_loop;
-		end if;
-	end loop;
-	close passengerListCursor;
-	
-	select cvs_string;
-end $$
-delimiter ;
+    -- variables for the cursor
+    DECLARE person_id VARCHAR(35);
+    DECLARE person_name VARCHAR(75);
+    DECLARE seat_row TINYINT(4);
+    DECLARE seat_num CHAR(1);
+    DECLARE seat_placement VARCHAR(15);
+
+    DECLARE cvs_string TEXT;
+
+    DECLARE done INT DEFAULT FALSE;
+
+    DECLARE passengerListCursor CURSOR FOR
+      SELECT
+        personID,
+        personName,
+        rowNumber,
+        seatNumber,
+        seatPlacement
+      FROM passengers
+        JOIN aircraftSeats ON passengers.seatID = aircraftSeats.seatID
+        JOIN aircrafts ON aircraftSeats.aircraftID = aircrafts.aircraftID
+        JOIN flights ON aircrafts.aircraftID = flights.aircraftID
+      WHERE flight_Number = flights.flightNumber AND flight_date = flights.flightDate;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    SELECT originatingAirport
+    FROM flightSchedules
+    WHERE flightNumber = flight_number
+    INTO flight_origin;
+
+    SELECT destinationAirport
+    FROM flightSchedules
+    WHERE flightNumber = flight_number
+    INTO flight_destination;
+
+    SET cvs_string = concat(flight_number, '_', flight_origin, '-', flight_destination, '_', flight_date, ':\n\n');
+
+    OPEN passengerListCursor;
+
+    read_loop: LOOP
+      FETCH passengerListCursor
+      INTO person_id, person_name, seat_row, seat_num, seat_placement;
+
+      SET cvs_string = concat(cvs_string, person_id, ';', person_name, ';', seat_row, seat_num, ';', seat_placement,
+                              ';\n');
+
+      IF done
+      THEN
+        LEAVE read_loop;
+      END IF;
+    END LOOP;
+    CLOSE passengerListCursor;
+
+    SELECT cvs_string;
+  END $$
+DELIMITER ;
 
 -- CALL PassengerList('FA501', '2014-09-19');
